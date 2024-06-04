d0b0e1d22025b43404a3cc041a16d391f590b074
Riizv <kchojnowski12@gmail.com>
2024-06-05 00:12:31 +0200

    still doesn't work

--- Matrix.h
+++ Matrix.h
@@ -6,30 +6,32 @@
 #define MATRIX_OPERATIONS_MATRIX_H
 #include <iostream>
 
-template<typename T>
+template<class X>
 class Matrix {
 public:
-    Matrix<T>(int wie, int kol);
-    Matrix<T>(const Matrix<T>& org);
+    Matrix(int wie, int kol);
+    Matrix(const Matrix& org);
     ~Matrix();
-    T& operator()( size_t i, size_t j);
-    Matrix<T> operator+(const Matrix& v) const;
-    Matrix<T> operator-(const Matrix& v) const;
-    Matrix<T>& operator=(const Matrix<T>& org);
-    Matrix<T> operator*(const Matrix& mat) const;
-    friend Matrix operator*(const double x, const Matrix& v);
+    X& operator()(size_t i, size_t j);
+    const X& operator()(size_t i, size_t j) const;
+    Matrix<X> operator+(const Matrix& v) const;
+    Matrix<X> operator-(const Matrix& v) const;
+    Matrix<X>& operator=(const Matrix<X>& org);
+    Matrix<X> operator*(const Matrix& mat) const;
+    template <typename U>
+    friend Matrix<U> operator*(const double x, const Matrix<U>& v);
 
     void state() const;
 private:
-    double **m;
+    X **m;
     int w, k;
 };
 
 template<typename T>
 Matrix<T>::Matrix(int wie, int kol) : w(wie), k(kol) {
-    m = new double *[wie];
+    m = new T *[wie];
     for (int i = 0; i < wie; i++)
-        m[i] = new double[kol];
+        m[i] = new T[kol];
 }
 
 template<typename T>
@@ -58,7 +60,14 @@ Matrix<T>::Matrix(const Matrix<T> &org) : w(org.w), k(org.k) {
 }
 
 template<typename T>
-T& Matrix<T>::operator()( size_t i, size_t j) {
+T& Matrix<T>::operator()(size_t i, size_t j) {
+    if (i < 1 || i > w || j < 1 || j > k)
+        throw std::out_of_range("Index out of range");
+    return m[i - 1][j - 1];
+}
+
+template<typename T>
+const T& Matrix<T>::operator()( size_t i, size_t j) const {
     if (i < 1 || i > w || j < 1 || j > k)
         throw std::out_of_range("Index out of range");
     return m[i - 1][j - 1];
@@ -88,15 +97,53 @@ Matrix<T> Matrix<T>::operator-(const Matrix &v) const {
 
 template<typename T>
 Matrix<T>& Matrix<T>::operator=(const Matrix<T>& org) {
+    if (this == &org)
+        return *this;
+
+    for (int i = 0; i < w; i++)
+        delete[] m[i];
+    delete[] m;
+
+    w = org.w;
+    k = org.k;
+    m = new T *[w];
+    for (int i = 0; i < w; i++)
+        m[i] = new T[k];
     for (int i = 0; i < w; i++)
         for (int j = 0; j < k; j++)
             m[i][j] = org.m[i][j];
+
     return *this;
 }
 
 template<typename T>
+Matrix<T> Matrix<T>::operator*(const Matrix& mat) const {
+    if (k != mat.w)
+        throw std::invalid_argument("Matrices size do not match for multiplication");
+    Matrix wyn(w, mat.k);
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < mat.k; j++) {
+            wyn.m[i][j] = 0;
+            for (int l = 0; l < k; l++) {
+                wyn.m[i][j] += m[i][l] * mat.m[l][j];
+            }
+        }
+    }
+    return wyn;
+}
+
+template<typename T>
+Matrix<T> operator*(const double x, const Matrix<T>& v) {
+    Matrix<T> wyn(v.w, v.k);
+    for (int i = 0; i < v.w; i++)
+        for (int j = 0; j < v.k; j++)
+            wyn.m[i][j] = x * v.m[i][j];
+    return wyn;
+}
+
+template<typename T>
 void Matrix<T>::state() const {
-    std::cout << "[ " << w << " , " << k << " ] \n";
+    std::cout << "[ " << w << " , " << k << " ]" << std::endl;
     for (int i = 0; i < w; i++) {
         for (int j = 0; j < k; j++) {
             std::cout << m[i][j] << "\t";
