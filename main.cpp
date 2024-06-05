8c33662b5fb8d2f432279ab6325a34796b08ce79
Riizv <kchojnowski12@gmail.com>
2024-06-06 00:09:59 +0200

    code formatting

--- main.cpp
+++ main.cpp
@@ -9,48 +9,49 @@ int main() {
     (*p).state();
     p->state();
 
-    std::cout << "Enter elements for mac1 (2x3 matrix):" << std::endl;
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            double x;
-            std::cin >> x;
-            mac1(i, j) = x;
-        }
-    }
-
-    std::cout << "Enter elements for mac2 (2x3 matrix):" << std::endl;
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            double x;
-            std::cin >> x;
-            mac2(i, j) = x;
-        }
-    }
-
-    std::cout << "mac1 state:" << std::endl;
-    mac1.state();
-
-    std::cout << "mac2 state:" << std::endl;
-    mac2.state();
-
-    // Perform matrix addition and print the result
-    mac3 = mac1 + mac2;
-    std::cout << "mac3 after addition (mac1 + mac2):" << std::endl;
-    mac3.state();
-
-    // Perform matrix multiplication and print the result
     try {
+
+        std::cout << "Enter elements for mac1 (2x3 matrix):" << std::endl;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                double x;
+                std::cin >> x;
+                mac1(i, j) = x;
+            }
+        }
+
+        std::cout << "Enter elements for mac2 (2x3 matrix):" << std::endl;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                double x;
+                std::cin >> x;
+                mac2(i, j) = x;
+            }
+        }
+        std::cout << "mac1 state:" << std::endl;
+        mac1.state();
+
+        std::cout << "mac2 state:" << std::endl;
+        mac2.state();
+
+        // Perform matrix addition and print the result
+        mac3 = mac1 + mac2;
+        std::cout << "mac3 after addition (mac1 + mac2):" << std::endl;
+        mac3.state();
+
         mac3 = mac1 * mac2;
         std::cout << "mac3 after multiplication (mac1 * mac2):" << std::endl;
         mac3.state();
+
+        // Scalar multiplication
+        mac3 = 2 * mac1;
+        std::cout << "mac3 after scalar multiplication (2 * mac1):" << std::endl;
+        mac3.state();
+
     } catch (const std::invalid_argument &e) {
-        std::cerr << "Matrix multiplication error: " << e.what() << std::endl;
+        std::cerr << "Matrix error: " << e.what() << std::endl;
     }
 
-    // Scalar multiplication
-    mac3 = 2 * mac1;
-    std::cout << "mac3 after scalar multiplication (2 * mac1):" << std::endl;
-    mac3.state();
 
     return 0;
 }
